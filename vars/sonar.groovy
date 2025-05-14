4c8fc24f63cac4427646aa8158cc19e5f06ed1d0
kishorebittu303 <kishorecloud303@gmail.com>
2025-05-14 11:33:35 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,6 +1,6 @@
-def call()
-        stage('sonar'){
-            steps{
-                sh "mvn clean package sonar:sonar"
-            }
-        }
+def call() {
+    stage('Sonar') {
+        // Note: no "steps" block here in shared library functions
+        sh "mvn clean package sonar:sonar"
+    }
+}
